ebf1d92fbda216a966ce259dc253243eb54fabbb
Unknown <danielthegray11@gmail.com>
2020-04-21 14:54:29 -0600

    Finished Part 1
    Completed part 1 of Project F. Added files-part1.txt

--- readfiles.py
+++ readfiles.py
@@ -1,15 +1,56 @@
-import sys
+import sys, sqlite3, os.path
 from os import listdir, walk
 from os.path import isfile, join
-import sqlite3
-import glob
+from zipfile import ZipFile
 
 if __name__ == '__main__':
     args = sys.argv
 
+def result(query):
+    with open('files-part1.txt', 'a') as output:
+        res = cur.execute(query)
+        # for fieldinfo in res.description:
+        #     output.write(fieldinfo[0])
+        for row in res:
+            output.write(str(row) + '\n')
+    #print (res.description)
 directory = args[1]
+print(directory)
+#directory = f"ProjectF\Stuff"
 
-onlyfiles = [f for f in listdir(directory) if isfile(join(directory,f))]
+with ZipFile(str(directory) + '.zip', 'r') as zipObj:
+    stufflist = zipObj.namelist()
 
-directories = [d for d in walk(directory) if d not in onlyfiles]
-    
+directories = []
+onlyfiles = []
+extensions = []
+cwd = os.getcwd()
+tempdir = cwd.split('\\')
+currentdir = '/'.join(tempdir)
+for item in stufflist:
+    temp = item.split('/')
+    fname = temp[-1]
+    ftemp = fname.split('.')
+    if len(ftemp) >= 2:
+        ext = ftemp[-1]
+    else:
+        ext = 'None'
+    #print(full, item)
+    temp2 = temp[0:len(temp)-1]
+    path = currentdir + '/' + '/'.join(temp2)
+    extensions.append(ext)
+    onlyfiles.append(fname)
+    directories.append(path)
+    #print(path)
+#print(onlyfiles[-7], 'and ', directories[-7], 'and ', extensions[-7])
+
+if os.path.exists('filesdb'):
+    os.remove('filesdb')
+
+conn = sqlite3.connect('filesdb')
+cur = conn.cursor()
+cur.execute("create table files (ext, path, fname)")
+for i in range(len(onlyfiles) - 1):
+    statement = "insert into files values(?, ?, ?)"
+    cur.execute(statement, (extensions[i], directories[i], onlyfiles[i]))
+results = result('select * from files')
